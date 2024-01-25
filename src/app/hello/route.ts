777cecd9effc3980001f4ba06309d24cc29a1454
Hiroya In <ing@digital-sorovider.jp>
2024-01-25 12:08:52 +0900

    ssr方式の変更

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const res = await fetch('https://meigen.doodlenote.net/api/json.php', { cache: "no-store" })
-    const [ data ] = await res.json()
+    const res = await fetch('https://worldtimeapi.org/api/timezone/Asia/Tokyo', { cache: "no-store" })
+    const data = (await res.json()) as { datetime: string };
 
     return NextResponse.json({
         messages: [
             'test',
             'これはダミーデータです',
             '/helloというエンドポイントをサンプルとして作成',
-            `現在時刻: ${(new Date()).toISOString()}`,
-            data.meigen
+            `現在時刻: ${data.datetime}`,
         ]
     })
 }
